2953766ac796e1486533141184b08a9079e8f874
lo1cgsan <lo1cgsan@gmail.com>
2022-10-13 11:01:12 +0200

    aplikacja wiadomości

--- projekt/settings.py
+++ projekt/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'osoby.apps.OsobyConfig'
+    'osoby.apps.OsobyConfig',
+    'wiadomosci.apps.WiadomosciConfig',
 ]
 
 MIDDLEWARE = [
---
+++ wiadomosci/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Wiadomosc(models.Model):
+    tresc = models.TextField("treść wiadomości")
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_d = models.DateTimeField("dodana", default=timezone.now)
+
+    class Meta:
+        verbose_name = 'wiadomość'
+        verbose_name_plural = 'wiadomości'
+        ordering = ['-data_d',]
+
+    def __str__(self):
+        return self.tresc[0:50] + f" ({self.autor.username})"
