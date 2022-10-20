1c566fa208398c0acdb67341ed5293d92fb5377b
lo1cgsan <lo1cgsan@gmail.com>
2022-10-20 10:35:18 +0200

    aplikacja wiadomości

--- wiadomosci/views.py
+++ wiadomosci/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from wiadomosci.models import Wiadomosc
+
+
+def lista_wiadomosci(request):
+    wiadomosci = Wiadomosc.objects.all()
+    kontekst = {'wiadomosci': wiadomosci}
+    return render(request, 'wiadomosci/lista_wiadomosci1.html', kontekst)
