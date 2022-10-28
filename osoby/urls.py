9590c3057c4232539993073d15fa747291fd3670
lo1cgsan <lo1cgsan@gmail.com>
2022-10-28 11:40:59 +0200

    dodanie formularza logowania

--- osoby/urls.py
+++ osoby/urls.py
@@ -6,4 +6,5 @@ app_name = 'osoby'
 urlpatterns = [
     path('', views.lista_absolwentow, name='lista'),
     path('about/', views.about, name='about'),
+    path('loguj/', views.loguj_osobe, name='loguj-osobe'),
 ]
--- osoby/views.py
+++ osoby/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+from osoby.forms import UserLoginForm
+
+
 def lista_absolwentow(request):
     return HttpResponse("<h1>Aplikacja osoby!</h1>")
 
 def about(request):
     return HttpResponse("<h1>Informacje o aplikacji</h1>")
+
+def loguj_osobe(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = UserLoginForm()
+    kontekst = {'form': form}
+    return render(request, 'osoby/loguj_osobe.html', kontekst)
